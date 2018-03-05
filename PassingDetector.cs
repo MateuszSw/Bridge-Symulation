a883863c0c9e4f68276448f2a035f73e91f54359
MateuszSw <kot12@DESKTOP-J28LDKJ>
2018-03-05 21:16:48 +0100

    Passing detector

---
+++ PassingDetector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassingDetector : MonoBehaviour
+{
+    public string vehicleTag;
+    public int Passed = 0;
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == vehicleTag)
+            Passed += 1;
+    }
+}
